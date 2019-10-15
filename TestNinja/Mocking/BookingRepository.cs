603b5544ec10e35f4bea27cbc17a5e506e32c71f
Виктор Блумрайтер <ska_vitek@mail.ru>
2019-10-15 22:38:54 +0300

    Testing BookingHelper class

---
+++ TestNinja/Mocking/BookingRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.Mocking
+{
+    public interface IBookingRepository
+    {
+        IQueryable<Booking> GetActiveBooking(int? exludedBookigId = null);
+    }
+
+    public class BookingRepository : IBookingRepository
+    {
+        public IQueryable<Booking> GetActiveBooking(int? exludedBookigId = null)
+        {
+            var unitOfWork = new UnitOfWork();
+            var bookings =
+                unitOfWork.Query<Booking>()
+                    .Where(
+                        b => b.Status != "Cancelled");
+
+            if (exludedBookigId.HasValue)
+            {
+                bookings = bookings.Where(b => b.Id != exludedBookigId.Value);
+            }
+
+            return bookings;
+        }
+    }
+}
